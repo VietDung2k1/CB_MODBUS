d8eb10133e84628c6007fff67fcc45997d824ebe
VietDung2k1 <truongdung755@gmail.com>
2023-06-30 13:00:18 +0700

    first commit

---
+++ Core/Inc/modbus_crc.h
@@ -0,0 +1,14 @@
+/*
+ * modbus_crc.h
+ *
+ *  Created on: Jun 30, 2023
+ *      Author: COTECON
+ */
+
+#ifndef INC_MODBUS_CRC_H_
+#define INC_MODBUS_CRC_H_
+#include "stdint.h"
+
+uint16_t crc16(uint8_t *buffer, uint16_t buffer_length);
+
+#endif /* INC_MODBUS_CRC_H_ */
